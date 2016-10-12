efd543a8418ad766fc5d6ff7b5a5fda42bcdf75d
Bruno Ribas <brunoribas@gmail.com>
2016-10-12 19:41:47 -0300

    sols/run-tests.sh: Permite execuções paralelas
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- sols/run-tests.sh
+++ sols/run-tests.sh
@@ -2,21 +2,24 @@
 
 INPUT=$1
 sample=$INPUT
+TMPFILE=$(mktemp)
 
 echo "--> $sample"
-printf "| %-15s | %7s | %7s | %10s | %-32s |\n" "Executavel" "Tam. MB" \
+printf "| %-15s | %7s | %10s | %10s | %-32s |\n" "Executavel" "Tam. MB" \
         "Tempo" "Score" "MD5 da Saida"
 for O in O0 O2 O3; do
   for bin in *.$O; do
     #if grep -q ssimples <<< "$bin"; then continue; fi
-    /usr/bin/time -f "%M %e" ./$bin < $sample > /tmp/sol 2> /tmp/tempo
-    read MEMORIA TEMPO < /tmp/tempo
-    ((MEMORIAMEGA=MEMORIA/1024))
+    /usr/bin/time -f "%M %e" ./$bin < $sample > $TMPFILE.sol 2> $TMPFILE.tempo
+    read MEMORIA TEMPO < $TMPFILE.tempo
+    MEMORIAMEGA=$(echo "scale=2;$MEMORIA/1024"|bc -l)
     #((MEMORIA*=1024))
     SCORE=$(echo "$MEMORIAMEGA*100*$TEMPO"|bc)
-    MD5="$(md5sum /tmp/sol|awk '{print $1}')"
-    printf "| %-15s | %7s | %7s | %10s | $MD5 |\n" "$bin"\
+    MD5="$(md5sum $TMPFILE.sol|awk '{print $1}')"
+    printf "| %-15s | %10s | %7s | %10s | $MD5 |\n" "$bin"\
             "$MEMORIAMEGA MB" "$TEMPO" "$SCORE"
   done
 done | sort -t'|' -k2 | sort -s -n -t'|' -k5
 echo
+
+rm $TMPFILE{,.sol,.tempo}
