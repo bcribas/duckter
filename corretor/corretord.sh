a2570cb34266b703e555e7982d1a8cd8e0355fe7
Bruno Ribas <brunoribas@gmail.com>
2016-11-06 15:44:30 -0200

    corretord.sh: Atua como um daemon agora
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- corretor/corretord.sh
+++ corretor/corretord.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
-HOJE=$(date +"%Y-%m-%d")
-TMPFILE=$(mktemp)
+while true; do
+  HOJE=$(date +"%Y-%m-%d")
+  echo "Sincronizando Fontes"
+  rsync -aHx recebetrabalho@trinium.naquadah.com.br:entrega/submissions/*.c exec/
 
-echo "Sincronizando Fontes"
-rsync -aHx recebetrabalho@trinium.naquadah.com.br:entrega/submissions/*.c exec/
+  echo "Enviar status de processando"
+  echo 'processing' > /tmp/status
+  scp /tmp/status recebetrabalho@trinium.naquadah.com.br:entrega/results/status.txt
 
-echo "Enviar status de processando"
-echo 'processing' > /tmp/status
-scp /tmp/status recebetrabalho@trinium.naquadah.com.br:entrega/results/status.txt
+  FIMSYNC=$(date +%s)
 
-FIMSYNC=$(date +%s)
+  make bin
 
-make bin
+  make hoje > tabelas/tabelas-$HOJE.t2t
 
-make hoje > tabelas/tabelas-$HOJE.t2t
-
-AGORA=$(date +%s)
-
-printf "Aguardando tempo minimo de 65s"
-while (( AGORA - FIMSYNC < 65 )); do
-  printf "."
-  sleep 5
   AGORA=$(date +%s)
+
+  printf "Aguardando tempo minimo de 65s"
+  while (( AGORA - FIMSYNC < 65 )); do
+    printf "."
+    sleep 5
+    AGORA=$(date +%s)
+  done
+  echo
+
+  echo 'idle' > /tmp/status.txt
+
+  NEXTROUND="$(date --date="3 hours")"
+  TOSLEEP=3h
+  if [[ "$(date --date="3 hours" +"%Y-%m-%d")" != "$HOJE" ]]; then
+    NEXTROUND="$(date --date="tomorrow 00:00:00")"
+    TOSLEEP=$(( $(date --date="tomorrow 00:00:00" +%s) - $AGORA +10))
+  fi
+
+  echo "$NEXTROUND" > /tmp/nextround.txt
+
+  rsync -aHx /tmp/status.txt /tmp/nextround.txt tabelas/tabelas-$HOJE.t2t recebetrabalho@trinium.naquadah.com.br:entrega/results
+
+  sleep $TOSLEEP
 done
-echo
-
-echo 'idle' > /tmp/status.txt
-
-date --date="3 hours" > /tmp/nextround.txt
-scp /tmp/status.txt /tmp/nextround.txt tabelas/tabelas-$HOJE.t2t recebetrabalho@trinium.naquadah.com.br:entrega/results
