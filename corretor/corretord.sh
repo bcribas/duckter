cd4b5a325e55d6ccf7f7cdf2d15dac9cc5facc0e
Bruno Ribas <brunoribas@gmail.com>
2016-11-06 14:57:37 -0200

    corretord.sh: Correções
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- corretor/corretord.sh
+++ corretor/corretord.sh
@@ -18,8 +18,8 @@ make hoje > tabelas/tabelas-$HOJE.t2t
 
 AGORA=$(date +%s)
 
-printf "Aguardando tempo minimo de 60s"
-while (( AGORA - FIMSYNC < 60 )); do
+printf "Aguardando tempo minimo de 65s"
+while (( AGORA - FIMSYNC < 65 )); do
   printf "."
   sleep 5
   AGORA=$(date +%s)
@@ -27,4 +27,6 @@ done
 echo
 
 echo 'idle' > /tmp/status.txt
-scp /tmp/status.txt tabelas/tabelas-$HOJE.t2t recebetrabalho@trinium.naquadah.com.br:entrega/results
+
+date --date="3 hours" > /tmp/nextround.txt
+scp /tmp/status.txt /tmp/nextround.txt tabelas/tabelas-$HOJE.t2t recebetrabalho@trinium.naquadah.com.br:entrega/results
