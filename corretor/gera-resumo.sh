2bf867107eff6a158e0caa1f62524a8dfb219c5d
Bruno Ribas <brunoribas@gmail.com>
2016-11-27 09:43:29 -0200

    gera-resumo.sh: Melhorado apresentação
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- corretor/gera-resumo.sh
+++ corretor/gera-resumo.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 make bin -k
+declare -a ENTRADAS
+ENTRADASCT=0
+
 for i in entradas/*in; do
   ARQ="$(basename $i .in)"
   SMALL="$(cut -d '-' -f 2- <<< "$ARQ")"
+  ENTRADAS[$ENTRADASCT]="$SMALL"
+  ((ENTRADASCT++))
   if [[ "$ARQ" == "$(date +"%Y-%m-%d")" ]]; then
     break
   fi
@@ -11,22 +16,6 @@ for i in entradas/*in; do
   cd ..
 done
 
-printf "\n\n\n"
-
-echo "=RESUMO GERAL ="
-TOTCOL=2
-printf "| %-35s |" "Nome"
-for i in entradas/*in; do
-  ARQ="$(basename $i .in)"
-  SMALL="$(cut -d '-' -f 2- <<< "$ARQ")"
-  if [[ "$ARQ" == "$(date +"%Y-%m-%d")" ]]; then
-    break
-  fi
-  printf " %8s |" "$SMALL"
-  ((TOTCOL++))
-done
-
-printf " %12s |\n" "Score Total"
 
 declare -A SCORESSIMPLES
 declare -A SCORESIMPLES
@@ -56,53 +45,89 @@ for bin in exec/ssimples.O0 exec/simples.O2; do
 
 done
 
-for bin in exec/*.e exec/*.epp exec/*O0 exec/*O2; do
-  BIN=$(basename $bin)
-  TOTSCORE=0
-  NOME=$BIN
-  if [[ -e "../entrega/passwd" ]]; then
-    N="$(cut -d'.' -f1 <<< "$BIN")"
-    NOME="$(grep "^$N:" ../entrega/passwd|cut -d':' -f3)"
-  fi
-  SCORE=0
-  if grep -q -i "simples" <<< "$BIN"; then
-    tput setab 7
-    tput setaf 0
-    tput bold
-  fi
-  SSIMPLESCORRETOS="0"
-  SIMPLESCORRETOS="0"
-  ERRADOS=0
-  printf "| %-35s |" "$NOME"
-  for in in entradas/*in; do
-    ARQ="$(basename $in )"
-    if [[ "$ARQ" == "$(date +"%Y-%m-%d").in" ]]; then
+declare -A SCORETOT
+
+for((k=0;k<=ENTRADASCT/7;k++)); do
+  printf "\n\n\n"
+  echo "=RESUMO GERAL pt.$k="
+  TOTCOL=2
+  tput bold
+  printf "| %-35s |" "Nome"
+  ((i=k*7));
+  ((fim=i+7));
+  while (( i<fim )) && (( i < ENTRADASCT )); do
+    ARQ="2016-${ENTRADAS[$i]}"
+    SMALL="$(cut -d '-' -f 2- <<< "$ARQ")"
+    if [[ "$ARQ" == "$(date +"%Y-%m-%d")" ]]; then
       break
     fi
-    #2016-11-12.in.ca1fb365.e.last.is
-    if test -e exec/$ARQ.$BIN.last.is && grep -q "correto" exec/$ARQ.$BIN.last.is; then
-      SCORE=$(awk -F '|' '{print $(NF-2)}' exec/$ARQ.$BIN.last)
-      SCORE="${SCORE// }"
-      TOTSCORE=$(echo "$TOTSCORE+$SCORE"|bc)
-      SSIMPLESCORRETOS+="+${SCORESSIMPLES[$ARQ]}"
-      SIMPLESCORRETOS+="+${SCORESIMPLES[$ARQ]}"
-    else
-      SCORE="- -"
-      ((ERRADOS++))
-      TOTSCORE=$(echo "$TOTSCORE+1000+${SCORESSIMPLES[$ARQ]}+${SCORESIMPLES[$ARQ]}"|bc)
-    fi
-    printf " %8s |" "$SCORE"
+    printf " %8s |" "$SMALL"
+    ((TOTCOL++))
+    ((i++))
   done
-  FIML=""
-  if grep -q -i "simples" <<< "$BIN"; then
-    FIML="$(tput sgr0)"
-  fi
-  if ((ERRADOS == 0 )); then
-    printf " %12s |$FIML\n" "$TOTSCORE"
+
+  if ((fim < ENTRADASCT-1 )); then
+    printf " %12s|\n" "Score Parcial"
   else
-    printf " %12s |$FIML\n" "$TOTSCORE"
+    printf " %12s |\n" "Score Total"
   fi
-done | sort -n -t '|' -k $((1+TOTCOL))
+  tput sgr 0
+
+
+  for bin in exec/*.e exec/*.epp exec/*O0 exec/*O2; do
+    BIN=$(basename $bin)
+    TOTSCORE=0
+    NOME=$BIN
+    if [[ -e "../entrega/passwd" ]]; then
+      N="$(cut -d'.' -f1 <<< "$BIN")"
+      NOME="$(grep "^$N:" ../entrega/passwd|cut -d':' -f3)"
+    fi
+    SCORE=0
+    if grep -q -i "simples" <<< "$BIN"; then
+      tput setab 7
+      tput setaf 0
+      tput bold
+    fi
+    SSIMPLESCORRETOS="0"
+    SIMPLESCORRETOS="0"
+    ERRADOS=0
+    printf "| %-35s |" "$NOME"
+    ((in=k*7));
+    ((fimin=in+7));
+    while (( in<fimin )) && (( in < ENTRADASCT )); do
+      ARQ="2016-${ENTRADAS[$in]}.in"
+      if [[ "$ARQ" == "$(date +"%Y-%m-%d").in" ]]; then
+        break
+      fi
+      #2016-11-12.in.ca1fb365.e.last.is
+      if test -e exec/$ARQ.$BIN.last.is && grep -q "correto" exec/$ARQ.$BIN.last.is; then
+        SCORE=$(awk -F '|' '{print $(NF-2)}' exec/$ARQ.$BIN.last)
+        SCORE="${SCORE// }"
+        TOTSCORE=$(echo "0${SCORETOT[$BIN]}+$SCORE"|bc)
+        SCORETOT[$BIN]=$TOTSCORE
+        SSIMPLESCORRETOS+="+${SCORESSIMPLES[$ARQ]}"
+        SIMPLESCORRETOS+="+${SCORESIMPLES[$ARQ]}"
+      else
+        SCORE="- -"
+        ((ERRADOS++))
+        TOTSCORE=$(echo "0${SCORETOT[$BIN]}+${SCORESSIMPLES[$ARQ]}/2+${SCORESSIMPLES[$ARQ]}"|bc)
+        SCORETOT[$BIN]=$TOTSCORE
+      fi
+      printf " %8s |" "$SCORE"
+      ((in++))
+    done
+    FIML=""
+    if grep -q -i "simples" <<< "$BIN"; then
+      FIML="$(tput sgr0)"
+    fi
+    if ((ERRADOS == 0 )); then
+      printf " %12s |$FIML\n" "${SCORETOT[$BIN]}"
+    else
+      printf " %12s |$FIML\n" "${SCORETOT[$BIN]}"
+    fi
+  done > /tmp/ordena
+  sort -n -t '|' -k $((1+TOTCOL)) /tmp/ordena
+done
 
 echo
 echo "-- Gerado em $(date -R)"
