9686f9a083c0b9538e1e07c7c670e828b0004e14
Bruno Ribas <brunoribas@gmail.com>
2016-12-13 21:38:10 -0200

    Adicionado cabeçalho GPL nos scripts
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- gerador/10-sample10-newday-craydays.sh
+++ gerador/10-sample10-newday-craydays.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#This file is part of DUCKTER.
+#
+#DUCKTER is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#DUCKTER is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with DUCKTER.  If not, see <http://www.gnu.org/licenses/>.
 
 POSSIBILIDADES=(q w e r t y u i o p a s d f g h j k l z x c v b n m Q W E R T Y U I O P A S D F G H J K L Z X C V B N M 1 2 3 4 5 6 7 8 9 0)
 POSSIBILIDADESTAM=${#POSSIBILIDADES[@]}
--- gerador/gerador-functions.sh
+++ gerador/gerador-functions.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#This file is part of DUCKTER.
+#
+#DUCKTER is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#DUCKTER is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with DUCKTER.  If not, see <http://www.gnu.org/licenses/>.
 
 if [[ ! -d lorem ]]; then
   git clone https://github.com/per9000/lorem.git
